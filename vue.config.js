3efec862b4a9222f5fcd30cfdc70bb7b5169aebd
xieyicheng <yicheng.xie@hand-china.com>
2026-01-20 15:17:03 +0800

    feat:配置host

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 9010
+    port: 9010,
+    // 端口（可选，避免冲突）
+    port: 8080,
+    // 关闭IPv6，解决Windows下IP识别问题
+    allowedHosts: 'all',
+    // 禁用IPv6
+    server: {
+      type: 'http',
+      options: {
+        ipv6Only: false
+      }
+    }
   }
 })
