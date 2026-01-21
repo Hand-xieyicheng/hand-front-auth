0a76f176598d4504adccbe9b410723b18a4adb05
xieyicheng <yicheng.xie@hand-china.com>
2026-01-21 16:34:44 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 9010,
-    // 端口（可选，避免冲突）
-    port: 8080,
     // 关闭IPv6，解决Windows下IP识别问题
     allowedHosts: 'all',
     // 禁用IPv6
